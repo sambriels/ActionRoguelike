b4defaf946d29443e9732916dc1ac01d1276085c
Sam Briels <sambriels@gmail.com>
2021-10-01 10:19:17 +0200

    Add target dummy and sound files

--- Source/ActionRoguelike/Private/SAttributeComponent.cpp
+++ Source/ActionRoguelike/Private/SAttributeComponent.cpp
@@ -4,6 +4,10 @@ USAttributeComponent::USAttributeComponent() {
   Health = 100;
 }
 
+bool USAttributeComponent::IsAlive() const {
+  return Health > 0.f;
+}
+
 bool USAttributeComponent::ApplyHealthChange(float Delta) {
   Health += Delta;
 
--- Source/ActionRoguelike/Public/SAttributeComponent.h
+++ Source/ActionRoguelike/Public/SAttributeComponent.h
@@ -30,6 +30,9 @@ protected:
   float Health;
 
 public:
+  UFUNCTION(BlueprintCallable)
+  bool IsAlive() const;
+
   UPROPERTY(BlueprintAssignable)
   FOnHealthChanged OnHealthChanged;
 
